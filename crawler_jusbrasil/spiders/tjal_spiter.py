94b298e9bdb7c38e42aab85def2ded540267f095
geovanecarvalho <geovaneoliveira.75@gmail.com>
2022-09-13 18:00:27 -0300

    data movimentacao

--- crawler_jusbrasil/spiders/tjal_spiter.py
+++ crawler_jusbrasil/spiders/tjal_spiter.py
@@ -6,6 +6,7 @@ class TjalSpiterPrimeiroGrau(scrapy.Spider):
     numero_processo = "0710802-55.2018.8.02.0001"
     start_urls = [f'https://www2.tjal.jus.br/cpopg/show.do?processo.codigo=01000O7550000&processo.foro=1&processo.numero={numero_processo}']
 
+
     def parse(self, response):
         classe = response.xpath('//*[@id="classeProcesso"]/text()').get()
         area = response.xpath('//*[@id="areaProcesso"]/span/text()').get()
@@ -18,12 +19,33 @@ class TjalSpiterPrimeiroGrau(scrapy.Spider):
         partes_advogado_autor = response.xpath('/html/body/div[2]/table[1]/tr[1]/td[2]/span/text()').get().strip()
         partes_re = response.xpath('/html/body/div[2]/table[1]/tr[2]/td/text()').getall()
         partes_advogados_re = response.xpath('/html/body/div[2]/table[1]/tr[2]/td/span/text()').getall()
+
+        data = response.xpath('//*[@id="tabelaTodasMovimentacoes"]/tr/td[1]/text()').getall()
+        title_movimentacao =  response.xpath('//*[@id="tabelaTodasMovimentacoes"]/tr/td[3]/text()').getall()
+        sub_title_movimetacao = response.xpath('//*[@id="tabelaTodasMovimentacoes"]/tr/td[3]/span/text()').getall()
+       
         
+        data_das_movimentacao = []
+        lista_title_movimentacao = []
+        lista_sub_title_movimentacao = []
+        
+
+        for l in title_movimentacao:
+            lista_title_movimentacao.append(l.strip().replace('\r\n', ''))
+
+        for l in sub_title_movimetacao:
+            lista_sub_title_movimentacao.append(l.strip().replace('\r\n',''))
+
+        
+        for d in data:
+            data_das_movimentacao.append({"data": d.strip()})
+
+
         autor = []
         re = []
         advogados_re = []
         lista_re = []
-       
+        
 
         for nome in partes_advogados_re:
             advogados_re.append(nome.strip()) 
@@ -49,8 +71,11 @@ class TjalSpiterPrimeiroGrau(scrapy.Spider):
             lista_re.append(advogados_re[i] + " " + re[i])
 
         lista_re[0] = lista_re[0][3:]
-        
+
         partes_do_processo = {'autor': autor, 'ré': lista_re}
+
+        lista_title_movimentacao_01 = [word.strip() for word in lista_title_movimentacao if word.strip() != "" ]
+        lista_sub_title_movimentacao_01 = [word.strip() for word in lista_sub_title_movimentacao if word.strip() != ""]
 
         yield {
             'classe': classe,
@@ -59,7 +84,8 @@ class TjalSpiterPrimeiroGrau(scrapy.Spider):
             'data_de_distribuicao': data_de_distribuicao,
             'juiz': juiz,
             'valor_da_acao':valor_da_acao,
-            'partes_do_processo': partes_do_processo
+            'partes_do_processo': partes_do_processo,
+            'lista_das_movimentacao': lista_sub_title_movimentacao_01
         }
 
    
