2d6d0c18c5b76ecdc0cbb7d282b545351da801fd
geovanecarvalho <geovaneoliveira.75@gmail.com>
2022-09-13 11:33:32 -0300

    create spyder tjal primeiro grau

--- crawler_jusbrasil/spiders/tjal_spiter.py
+++ crawler_jusbrasil/spiders/tjal_spiter.py
@@ -7,9 +7,61 @@ class TjalSpiterPrimeiroGrau(scrapy.Spider):
     start_urls = [f'https://www2.tjal.jus.br/cpopg/show.do?processo.codigo=01000O7550000&processo.foro=1&processo.numero={numero_processo}']
 
     def parse(self, response):
-        pass
+        classe = response.xpath('//*[@id="classeProcesso"]/text()').get()
+        area = response.xpath('//*[@id="areaProcesso"]/span/text()').get()
+        assunto = response.xpath('//*[@id="assuntoProcesso"]/text()').get()
+        data_de_distribuicao = response.xpath('//*[@id="dataHoraDistribuicaoProcesso"]/text()').get()
+        juiz = response.xpath('//*[@id="juizProcesso"]/text()').get()
+        valor_da_acao = response.xpath('//*[@id="valorAcaoProcesso"]/text()').get().replace(" ", "")
 
+        partes_autor = response.xpath('/html/body/div[2]/table[1]/tr[1]/td[2]/text()').getall()
+        partes_advogado_autor = response.xpath('/html/body/div[2]/table[1]/tr[1]/td[2]/span/text()').get().strip()
+        partes_re = response.xpath('/html/body/div[2]/table[1]/tr[2]/td/text()').getall()
+        partes_advogados_re = response.xpath('/html/body/div[2]/table[1]/tr[2]/td/span/text()').getall()
         
+        autor = []
+        re = []
+        advogados_re = []
+        lista_re = []
+       
 
+        for nome in partes_advogados_re:
+            advogados_re.append(nome.strip()) 
+            
+    
+        for nome in partes_re:
+            re.append(nome.strip())
+
+        for nome in partes_autor:
+            autor.append(nome.strip())
+        
+        autor.pop(1)
+
+        autor[1] = partes_advogado_autor + " " + autor[1]
+        re.pop(0)
+        re.pop(0)
+        re.pop(1)
+        re.pop(2)
+        re.pop(3)
+        re.pop(4)
+
+        for i in range(len(re)):
+            lista_re.append(advogados_re[i] + " " + re[i])
+
+        lista_re[0] = lista_re[0][3:]
+        
+        partes_do_processo = {'autor': autor, 'ré': lista_re}
+
+        yield {
+            'classe': classe,
+            'area': area,
+            'assunto': assunto,
+            'data_de_distribuicao': data_de_distribuicao,
+            'juiz': juiz,
+            'valor_da_acao':valor_da_acao,
+            'partes_do_processo': partes_do_processo
+        }
+
+   
     
 
