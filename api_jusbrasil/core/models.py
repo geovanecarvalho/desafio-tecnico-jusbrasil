478ef92be55e573bae63a34ab5cd707c66de231a
geovanecarvalho <geovaneoliveira.75@gmail.com>
2022-09-15 19:57:45 -0300

    install django rest framework

---
+++ api_jusbrasil/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Crawler(models.Model):
+    numero_processo = models.CharField(primary_key=True, max_length=25)
+    classe = models.CharField(max_length=50, blank=True, null=True)
+    area = models.CharField(max_length=50, blank=True, null=True)
+    assunto = models.CharField(max_length=50, blank=True, null=True)
+    data_de_distribuicao = models.CharField(max_length=50, blank=True, null=True)
+    juiz = models.CharField(max_length=50, blank=True, null=True)
+    valor_da_acao = models.CharField(max_length=50, blank=True, null=True)
+    partes_do_processo = models.TextField(blank=True, null=True)
+    lista_das_movimentacao = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'crawler'
+
+    def __str__(self):
+        return self.numero_processo
